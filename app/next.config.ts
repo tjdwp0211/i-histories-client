69eb76eff3e06e91e1d4b2c7a0c357549f9456e0
tjdwp0211 <rlatjdwp0211@naver.com>
2025-03-06 23:55:29 +0900

    build(package): @emotion
    - emotion으로 스타일 라이브러리 변경
    - 기존 @vanilla-extract 삭제
    - dev script에 --turbo 추가

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
-const withVanillaExtract = createVanillaExtractPlugin({
-  identifiers: ({ hash }) => `prefix_${hash}`,
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
 };
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
