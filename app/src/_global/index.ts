2951fd1ff54fcbf79ba17de52c4ee46c121908e9
tjdwp0211 <rlatjdwp0211@naver.com>
2025-03-24 22:44:37 +0900

    global(layout) root layout
    - font, rootStyle, StyledComponentRegistry 적용

---
+++ app/src/_global/index.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import RootStyle from "./lib/styledComponents/RootStyle";
+import StyledComponentsRegistry from "./lib/styledComponents/Registry";
+import GlobalThemeProvider from "./provider/ThemeProvier";
+import { ibmPlex } from "./fonts";
+
+export { RootStyle, StyledComponentsRegistry, GlobalThemeProvider, ibmPlex };
